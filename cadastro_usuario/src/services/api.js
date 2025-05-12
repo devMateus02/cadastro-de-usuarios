81473823a70bf369bb9fd3005dd294037d7af88e
devMateus02 <mateusnascimentocelestino@gmail.com>
2025-05-12 16:58:25 -0300

    Update api.js

--- cadastro_usuario/src/services/api.js
+++ cadastro_usuario/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://cadastro-de-usuario-zjsy.onrender.com'
+    baseURL: 'https://cadastro-de-usuarios-4mr5.onrender.com'
 })
 
 
